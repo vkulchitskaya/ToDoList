7a8afd0e569c3449b8509ab26bbde065cba6de45
Valeriya Kulchitskaya <arealise@gmail.com>
2017-02-02 18:05:05 +0300

    add AddTask

--- script.js
+++ script.js
@@ -12,13 +12,18 @@ class Task {
 }
 
 /*СОЗДАЕМ ДЛЯ ОТЛАДКИ ЭКЗЕМЛЯР КЛАССА ЗАДАЧА, ЧТОБЫ ПОСМОТРЕТЬ, ЧТО ВСЕ ПРОСТО РАБОТАЕТ*/
-var task = new Task("Текст задачи");
+
 
 
 /*ПО НАЖАТИЮ КНОПКИ НАДО СОЗДАТЬ ЭКЗЕМЛЯР ЗАДАЧИ, И ЗАСУНУТЬ ЕГО В КОЛЛЕКЦИЮ ЗАДАЧ
 НО ПОКА ЗДЕСЬ ПРОСТО ДИАЛОГОВОЕ ОКНО*/
+var Tittle,task 
+
 function AddTask(){
-	alert( "Здесь могла бы быть Ваша задача" );
+	Tittle = document.getElementById("TaskTittle").value;
+    task = new Task(Tittle);
+	alert("Задача создалась!!!");
+	console.log(task);
 }
 
 
@@ -34,8 +39,5 @@ TaskCollection[1] = {
 }
 
 
-	
-console.log(task);
-console.log(TaskCollection);
 
 
