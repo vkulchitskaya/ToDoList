71307fdb9490a9382119eb666e7f0b4bcd7cce28
Valeriya Kulchitskaya <arealise@gmail.com>
2017-02-02 16:56:12 +0300

    add class Task

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-class Task{
-	this.name=name
+/*class Task{
+	this.name=name;
 }
 
-var task = new Task("Написать ToDoList")
+var task = new Task("Суперважное дело");
+
+var task="тест";*/
+
 
 
 
 
 /*начни с моделей - классов задач
 чтобы ты просто в консоли браузера могла создавать новые задачи
+
+
 мотом сделай класс для хранения списка задач
 потом напиши под, который просто покажет этот список на экране
 добавь форму, которая в этот список позволит добавлять новую задачу
@@ -16,3 +21,29 @@ var task = new Task("Написать ToDoList")
 
 
 Пишите в консоли myfunc({a: 12, b: 24}), и, о чудо, вы передали в функцию данные, введенные пользователем:)*/
+
+/*Task({name:"Суперважное дело"})
+
+
+
+
+*/
+'use strict';
+
+class Task {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  writeTask() {
+    alert(this.name);
+  }
+
+}
+
+var task = new Task("Текст задачи");
+task.writeTask(); // Текст задачи
+
+
+console.log(task);
