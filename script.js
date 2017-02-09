2a786f92b9cd690b37075f6591386a3f81d6cb4b
Valeriya Kulchitskaya <arealise@gmail.com>
2017-02-09 16:33:04 +0300

    add event to View

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+class Task {
+  constructor(name) {
+    this.name = name;
+  } 
+  writeTask() {
+    alert(this.name);
+  }
+}
+
 class TaskCollection{
 
 	constructor() {
 		this.taskCollection = [];
+	}
+	addTask(task){
+		this.taskCollection.push(task); 
+	}
+	removeTask(task) {
+		this.removeTaskByName(task.name);
+	}
+	removeTaskByName(name) {
+		var i = this.taskCollection.indexOf(name)
+		this.taskCollection.splice(i, 1); // начиная с позиции 1, удалить 1 элемент
+	}
+	getTasks() {
+		return this.taskCollection;
+	}
 }
-}
+
+
+
+
 
 class View{
 	constructor(idField,idButton,idButtonDis,idUl){
@@ -11,15 +37,24 @@ class View{
 		this.idButton=document.getElementById(idButton);
 		this.idButtonDis=document.getElementById(idButtonDis);
 		this.idUl=document.getElementById(idUl);
-}
+		this.idButton.onclick = function(){
+			var fieldValue = document.getElementById(idField).value;
+			var task = new Task(fieldValue);
+			/*нажатие кнопки создает задачу, но не знаю, как положить эту задачу в коллекцию*/
+			/*application.taskCollection.addTask(task);*/
+		}
+		}
 }
 
 class Application{
 	constructor(){
-		this.view = new View('taskTittle','addButton','displayButton','listTask');
 		this.taskCollection = new TaskCollection();
+		this.view = new View('taskTittle','addButton','displayButton','listTask');		
 	}
 }
 
+
+window.onload = function(){
 var application = new Application();
 console.log(application);
+};
