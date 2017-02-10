b332dbf876a59a6296089ba6fb5f91b8c4b3ae12
Valeriya Kulchitskaya <arealise@gmail.com>
2017-02-10 16:02:41 +0300

    save application and controller

--- script.js
+++ script.js
@@ -29,51 +29,46 @@ class TaskCollection{
 
 
 
-
-
 class View{
 	
-	constructor(idField,idButton,idButtonDis,idUl,taskCollection){
+	constructor(idField,idButton,idButtonDis,idUl/*,taskCollection*/){
 		this.idField=document.getElementById(idField);
 		this.idButton=document.getElementById(idButton);
 		this.idButtonDis=document.getElementById(idButtonDis);
+		this.idFieldValue=document.getElementById(idField).value;
 		this.idUl=document.getElementById(idUl);
 		this.idButton.onclick = function(){
 			var fieldValue = document.getElementById(idField).value;
 			var task = new Task(fieldValue);
-			taskCollection.addTask(task);}
+			/*taskCollection.addTask(task);*/}
 		this.idButtonDis.onclick = function(){
-			console.log(taskCollection);}
-
+			/*console.log(taskCollection);*/}
 		}
-
+		getFieldValue(){
+			return document.getElementById(idField).value;
+		}
 }
 
-/*class Application{
+class Application{
 	constructor(){
 		this.taskCollection = new TaskCollection();
-		this.view = new View('taskTittle','addButton','displayButton','listTask', this.taskCollection);		
+		this.view = new View('taskTittle','addButton','displayButton','listTask');
+		this.controller = new Controller(this.view,this.taskCollection);
 	}
 }
 
+class Controller{
+	constructor(view,taskCollection){
+		}
+	}
+
+
 window.onload = function(){
+
 var application = new Application();
 console.log(application);
-};*/
 
-
-window.onload = function(){
-var taskCollection = new TaskCollection();
-var view = new View('taskTittle','addButton','displayButton','listTask', taskCollection);
-var controller = new Controller(view,taskCollection);
-console.log(controller);
 };
 
 
 
-class Controller{
-	constructor(view,taskCollection){
-		this.taskCollection = taskCollection;
-		this.view = view;
-	}
-}
